41ea346d2d2e913f6665332d54f070cbd998adb0
Aurelien Benel <aurelien.benel@utt.fr>
2022-11-27 21:03:07 +0100

    REQUIREMENT: Source and scholia are documented.
    Displayed for now: 
    title, creator, translators, volume name and issue year.

--- backend/src/views/links/map.js
+++ backend/src/views/links/map.js
@@ -1,8 +1,18 @@
-function ({_id, text = '', links = [], isPartOf = _id}) {
+function (doc) {
+  let { _id, text = '', links = [], isPartOf = _id } = doc;
+  let metadata = Object.fromEntries(
+    Object.entries(doc)
+      .filter(x => x[0].startsWith('dc_'))
+  );
   let related =  new Set(
     links.reduce((l, {object, subject}) => [...l, subject, object], [])
       .filter(x => !!x)
   ).add(isPartOf);
+  if (Object.keys(metadata).length) {
+    related.forEach(x => {
+      emit([x, 0], Object.assign({id: isPartOf}, metadata));
+    });
+  }
   const PASSAGE = /{(.+)} ([^{]+)/g;
   [...text.matchAll(PASSAGE)].forEach(([_, rubric, passage]) => {
  	  related.forEach(x => {
--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -10,6 +10,7 @@ function Page() {
 
   const [page, setPage] = useState([]);
   const [margin, setMargin] = useState();
+  const [metadata, setMetadata] = useState([]);
   let {id = "02ee00d85cdb11ed834c4fb9e3c972af"} = useParams();
 
   useEffect(() => {
@@ -17,8 +18,17 @@ function Page() {
       .then(x => x.json())
       .then(
         (result) => {
+          let { metadata, data } = result.rows.reduce(({metadata, data}, x) => {
+            if (x.key[1] === 0) {
+              metadata.push(x.value);
+            } else {
+              data.push(x);
+            }
+            return {metadata, data};
+          }, {metadata: [], data: []});
+          setMetadata(metadata);
           setMargin(null);
-          let passages = result.rows.reduce(({whole, part}, x, i, {length}) => {
+          let passages = data.reduce(({whole, part}, x, i, {length}) => {
             if (part.rubric && (x.key[1] !== part.rubric)) {
               whole.push(part);
               part = {source:'', scholia:[]};
@@ -45,6 +55,10 @@ function Page() {
 
   return (
       <Container className="page">
+        <Row>
+          <RunningHeadSource metadata={ metadata.find(x => (x.id === id)) } />
+          <RunningHeadMargin metadata={ metadata.find(x => (x.id === margin)) } />
+        </Row>
         {page.map(({rubric, source, scholia}) =>
           <Passage key={rubric} source={source} rubric={rubric} scholium={scholia[0]} hasMargin={!!margin} />)}
       </Container>
@@ -75,6 +89,38 @@ function PassageMargin({active, text}) {
     <Col xs={5} className="scholium">
       {text}
     </Col>
+  );
+}
+
+function RunningHeadSource({metadata}) {
+  return (
+    <Col xs={7} className="source">
+      <RunningHead metadata={metadata} />
+    </Col>
+  );
+}
+
+function RunningHeadMargin({metadata}) {
+  if (!metadata) return;
+  return (
+    <Col xs={5} className="scholium">
+      <RunningHead metadata={metadata} />
+    </Col>
+  );
+}
+
+function RunningHead({metadata}) {
+  return (
+    <>
+      <span className="runningHead work">
+        {metadata?.dc_title} ({metadata?.dc_creator}),
+      </span>
+      <span className="runningHead edition">
+        {metadata?.dc_translator? `Translated by ${metadata.dc_translator.join(' & ')}` : ''}
+        {metadata?.dc_isPartOf? <i>{metadata.dc_isPartOf}</i> : ''}
+        {metadata?.dc_issued? `, ${new Date(metadata.dc_issued).getFullYear()}` : ''}
+      </span>
+    </>
   );
 }
 
