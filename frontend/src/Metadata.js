2bebf48c066a3b1183cc9deebc4284820ef9a833
Aurelien Benel <aurelien.benel@utt.fr>
2023-02-13 17:48:34 +0100

    FIX: A document may not be a part of another work.

--- frontend/src/Metadata.js
+++ frontend/src/Metadata.js
@@ -9,9 +9,9 @@ function Metadata({metadata}) {
           {dc_title} {dc_creator ? `(${dc_creator})` : ''},
         </span>
         <span className="edition">
-          {dc_translator ? `Translated by ${dc_translator.join(' & ')}` : ''}
-          {dc_isPartOf ? <i>{metadata.dc_isPartOf}</i> : ''}
-          {dc_issued ? `, ${new Date(dc_issued).getFullYear()}` : ''}
+          {dc_translator ? `Translated by ${dc_translator.join(' & ')}, ` : ''}
+          {dc_isPartOf ? <i>{metadata.dc_isPartOf}, </i> : ''}
+          {dc_issued ? `${new Date(dc_issued).getFullYear()}` : ''}
         </span>
       </>
     );
