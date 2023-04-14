49ddd2a19a26d263bcdfe83d45bef29e2dae2eb4
Agathe ALLIROL--BINET <agathe.allirol__binet@utt.fr>
2023-04-14 09:49:37 +0200

    FIX: A copyright (or license) notice should be displayed (fixes #50).
    In this commit, we solve the fix 50 by displaying the document license in its title.
    
    Co-authored-by: AVCharpentier <antoine_valentin.charpentier@utt.fr>
    Co-authored-by: marina-pera <marina.peralta_fonseca@utt.fr>
    Co-authored-by: astrid-devericourt <astrid.royer_de_vericourt@utt.fr>
    Co-authored-by: cug-sun <sun.sun@utt.fr>

--- frontend/src/Metadata.js
+++ frontend/src/Metadata.js
@@ -13,7 +13,25 @@ function Metadata({metadata}) {
           {dc_isPartOf ? <i>{metadata.dc_isPartOf}, </i> : ''}
           {dc_issued ? `${new Date(dc_issued).getFullYear()}` : ''}
         </span>
+        , <License metadata={metadata} />
       </>
+    );
+  }
+}
+
+function License({metadata}) {
+  let {dc_license} = metadata;
+  let license = 'All rights reserved';
+  if ( dc_license ) {
+    const licenseList = ["by-nc-sa", "by-nc-nd", "by-sa", "by-nd", "by-nc", "by"];
+    let licenseStocked = licenseList.find(license => dc_license.includes(license));
+    if ( licenseStocked ) {
+      license = licenseStocked;
+    }
+    return (
+      <span className="license">
+        {license}
+      </span>
     );
   }
 }
