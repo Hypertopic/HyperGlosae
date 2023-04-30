Hypertopic/HyperGlosae

commit fe977d1fdcc7ca55a87ca9a47995d03322f77f40

FIX: Included images should be sourced (fixes #21).

frontend/src/CroppedImage.js
@@ -1,19 +1,29 @@
 import ReactCrop from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
 
-function CroppedImage({src, alt}) {
+function CroppedImage({ src, alt, title }) {
+
+  const image = <img className="figure-img img-fluid rounded" {...{ src, alt }} />
+  const caption = <figcaption className="figure-caption text-end">{title}</figcaption>
+
   let fragment = src.match(/#xywh=percent:([.\d]+),([.\d]+),([.\d]+),([.\d]+)/);
   if (fragment) {
     let [_, x, y, width, height] = fragment;
-    let crop = {unit: '%', x, y, width, height};
+    let crop = { unit: '%', x, y, width, height };
     return (
-      <ReactCrop {...{crop}} disabled={true} >
-        <img {...{src, alt}} />
-      </ReactCrop>
+      <figure className="figure">
+        <ReactCrop {...{ crop }} disabled={true} >
+          {image}
+        </ReactCrop>
+        {caption}
+      </figure>
     );
   }
   return (
-    <img {...{src, alt}} />
+    <figure className="figure">
+      {image}
+      {caption}
+    </figure>
   );
 }
 

frontend/src/Page.js
@@ -66,12 +66,9 @@ function Page() {
     if (!doc) {
       return value.text;
     }
-    if (value.inclusion !== 'whole') {
-      let fragment = '#' + value.inclusion;
-      let imageReference = /!\[[^\]]*\]\([^)]+/;
-      return doc.text.replace(imageReference, '$&' + fragment);
-    }
-    return doc.text;
+    let fragment = (value.inclusion !== 'whole' ? '#' + value.inclusion : "") + ` "${doc.dc_title}"`;
+    let imageReference = /!\[[^\]]*\]\([^)]+/;
+    return doc.text.replace(imageReference, '$&' + fragment);
   }
 
   useEffect(() => {
@@ -129,7 +126,7 @@ function Passage({source, rubric, scholia, margin}) {
       <Col className="main">
         <Container>
           <Row>
-            <Col>
+            <Col className="figure-body">
               <FormattedText>
                 {source}
               </FormattedText>

frontend/src/index.css
@@ -15,3 +15,12 @@ code {
 .screen {
   margin: 12px 0;
 }
+
+.figure {
+  margin-bottom: 15px;
+  margin-top: 15px;
+}
+
+.figure-body {
+  margin-top: 20px !important;
+}

commit 646dd7bf380c36f87bb9381ef2e4f8188641c443

FIX: Image fragments should not be offset.
Regression caused by fe977d1fdcc7ca55a87ca9a47995d03322f77f40.

frontend/src/CroppedImage.js
@@ -3,7 +3,7 @@ import 'react-image-crop/dist/ReactCrop.css';
 
 function CroppedImage({ src, alt, title }) {
 
-  const image = <img className="figure-img img-fluid rounded" {...{ src, alt }} />;
+  const image = <img className="img-fluid rounded" {...{ src, alt }} />;
   const caption = <figcaption className="figure-caption text-end">{title}</figcaption>;
 
   let fragment = src.match(/#xywh=percent:([.\d]+),([.\d]+),([.\d]+),([.\d]+)/);