30f671003068fbaae857127e84a33c7f102038e0
SUN Sun <sun.sun@utt.fr>
2023-05-05 10:25:46 +0200

    FEATURE: Include a YouTube video into a document (closes #68).
    As the documentation of react-markdown says, we can change the things that come from markdown. I tried like this:
    
    1.Edit the youtube video link in Markdown link syntax in a sample file
    
    2.In FormattedText.js, add a key `a` in props components of ReactMarkdown, set its value with a custom method.
    
    3.In custom method, replace the html element <a> with <iframe>. To do this, define a regex to filter the youtube video id, and build the iframe.
    
    Co-authored-by: astrid-devericourt <astrid.royer_de_vericourt@utt.fr>
    Co-authored-by: GagatheRollex <agathe.allirol__binet@utt.fr>"

--- frontend/src/FormattedText.js
+++ frontend/src/FormattedText.js
@@ -10,7 +10,6 @@ function FormattedText({children}) {
       components={{
         img: CroppedImage,
         a: ({children, href}) => <a href={href}>{children}</a>
-
       }}
       remarkRehypeOptions={{
         handlers: defListHastHandlers
@@ -18,6 +17,25 @@ function FormattedText({children}) {
     >
       {children}
     </ReactMarkdown>
+  );
+}
+
+function getId(text) {
+  const regExp = /^.*(?:youtube\.com\/watch\?v=|youtu\.be\/)([^\s&]{11})/;
+  const match = text.match(regExp);
+  return match ? match[1] : null;
+}
+
+function embedVideo({href, children}) {
+  const videoId = getId(href);
+  if (videoId) {
+    const embedLink = `https://www.youtube.com/embed/${videoId}`;
+    return (
+      <iframe width="80%" height="300" src={embedLink} frameBorder="0" allowFullScreen></iframe>
+    );
+  }
+  return (
+    <a href={href}>{children}</a>
   );
 }
 
