267dea63fb1162aa1032bc084c250ed0f15f1dd9
chen jiashun <exia1chen@gmail.com>
2024-04-17 18:11:25 +0200

    select text and popup a button in FormattedText.js

--- frontend/src/components/FormattedText.js
+++ frontend/src/components/FormattedText.js
@@ -3,22 +3,30 @@ import remarkUnwrapImages from 'remark-unwrap-images';
 import { remarkDefinitionList, defListHastHandlers } from 'remark-definition-list';
 import CroppedImage from './CroppedImage';
 import VideoComment from './VideoComment';
+import SelectPopUp from './SelectPopUp';
+import {useState} from 'react';
 
 function FormattedText({children}) {
+
+  const [selectedText, setSelectedText] = useState('');
   return (
-    <ReactMarkdown
-      remarkPlugins={[remarkDefinitionList, remarkUnwrapImages]}
-      components={{
-        img: (x) => embedVideo(x) || CroppedImage(x),
-        p: (x) => VideoComment(x) || <p>{x.children}</p>,
-        a: ({children, href}) => <a href={href}>{children}</a>
-      }}
-      remarkRehypeOptions={{
-        handlers: defListHastHandlers
-      }}
-    >
-      {children}
-    </ReactMarkdown>
+    <div>{selectedText && <SelectPopUp selectedText={selectedText} />}
+      <ReactMarkdown
+        remarkPlugins={[remarkDefinitionList, remarkUnwrapImages]}
+        components={{
+          img: (x) => embedVideo(x) || CroppedImage(x),
+          p: (x) => VideoComment(x) || (
+            <p onMouseUp={() => setSelectedText(window.getSelection().toString())}>{x.children}</p>
+          ),
+          a: ({children, href}) => <a href={href}>{children}</a>
+        }}
+        remarkRehypeOptions={{
+          handlers: defListHastHandlers
+        }}
+      >
+        {children}
+      </ReactMarkdown>
+    </div>
   );
 }
 
