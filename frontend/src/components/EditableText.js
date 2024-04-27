f3832d57801b21c816bdb0803aca1794bead36a1
Aurelien Benel <aurelien.benel@utt.fr>
2024-04-27 14:13:46 +0200

    REFACTORING: React states should not depend on props.
    If `text` had changed, `editedDocument` would not have been updated
    properly.
    
    While no bug had been reported, this prevented ticket #161 to be fixed.

--- frontend/src/components/EditableText.js
+++ frontend/src/components/EditableText.js
@@ -5,38 +5,52 @@ import FormattedText from './FormattedText';
 
 function EditableText({id, text, rubric, backend, setLastUpdate}) {
   const [beingEdited, setBeingEdited] = useState(false);
-  const [editedDocument, setEditedDocument] = useState({
-    text: (rubric) ? `{${rubric}} ${text}` : text
-  });
+  const [editedDocument, setEditedDocument] = useState();
+  const [editedText, setEditedText] = useState();
   const PASSAGE = new RegExp(`\\{${rubric}} ?([^{]+)`);
+  const FIRST_PASSAGE = new RegExp('\\{[^}]+} ?([^{]+)');
+
+  let parsePassage = (rawText) => (rubric)
+    ? rawText.match(PASSAGE)[1]
+    : rawText;
+
+  let parseFirstPassage = (rawText) => {
+    let parsed = rawText.match(FIRST_PASSAGE);
+    return (parsed) ? parsed[1] : rawText;
+  };
 
   let handleClick = () => {
     setBeingEdited(true);
     backend.getDocument(id)
       .then((x) => {
         setEditedDocument(x);
+        setEditedText(parsePassage(x.text));
       });
   };
 
   let handleChange = (event) => {
-    let editedText = (rubric)
-      ? editedDocument.text.replace(PASSAGE, `{${rubric}} ${event.target.value}`)
-      : event.target.value;
-    setEditedDocument({ ...editedDocument, text: editedText });
+    setEditedText(event.target.value);
   };
 
   let handleBlur = () => {
-    backend.putDocument(editedDocument)
+    let parsedText = parseFirstPassage(editedText);
+    let text = (rubric)
+      ? editedDocument.text.replace(PASSAGE, `{${rubric}} ${parsedText}`)
+      : editedText;
+    backend.putDocument({ ...editedDocument, text })
+      .then(x => {
+        setEditedText(parsedText);
+        return x;
+      })
       .then(x => setLastUpdate(x.rev))
       .then(() => setBeingEdited(false))
       .catch(console.error);
   };
 
-  let editedText = (rubric) ? editedDocument.text.match(PASSAGE)[1] : editedDocument.text;
   if (!beingEdited) return (
     <div className="editable content" onClick={handleClick} title="Edit content...">
       <FormattedText>
-        {editedText}
+        {editedText || id && text}
       </FormattedText>
     </div>
   );
