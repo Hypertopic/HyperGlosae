55a99791dd1a064406cb1a712350b9edb7b2a4cb
Aurelien Benel <aurelien.benel@utt.fr>
2024-06-02 21:07:36 +0200

    PROCESS: Refactored test step to be faster and more robust.

--- features/step_definitions/context.rb
+++ features/step_definitions/context.rb
@@ -3,8 +3,26 @@ Soit('la liste des documents affichée') do
 end
 
 Soit('{string} le document principal') do |title|
-  visit '/'
-  click_on_icon_next_to('focus', title)
+  case title
+    when "Les fées (Charles Perrault)"
+      visit '/37b4b9ba5cdb11ed887beb5c373fa643'
+    when "Analyse de l'entretien"
+      visit '/c2b9f52285ce11edbd0aff9b25defbab'
+    when "Photographie : vitrail, baie 113, Église Saint-Nizier, Troyes"
+      visit '/b8cc79d8abba11edb9ee53989bc96c06'
+    when "A tündérek (Charles Perrault)"
+      visit '/09c906c6732b11ed89466ba197585f87'
+    when "Étiquetage de l'entretien"
+      visit '/6327c5008d1f11ed9aa8e7ae771dee2e'
+    when "Soleil noir et étoiles qui tombent – Comparaison de vitraux"
+      visit '/a0cdf96ab2c211ed9f5ecfb8095a0b31'
+    when "Vidéo Sherlock Jr. (Buster Keaton)"
+      visit '/4e1a31e14b032f2fa9e161ee9b009125'
+    when "Treignes, le 8 septembre 2012 (Christophe Lejeune)"
+      visit '/6b56ee657c870dfacd34e9ae4e0643dd'
+    when "Entretien avec un responsable d'opération"
+      visit '/05b61f5285c711ed97bf6b9b56808c45'
+  end
 end
 
 Soit('{string} une des sources') do |title|
