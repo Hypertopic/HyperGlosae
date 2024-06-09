e86405544e90b7e1ee8805bb09670351c5fc8dc1
Aurelien Benel <aurelien.benel@utt.fr>
2024-06-09 14:27:31 +0200

    PROCESS: Reduced timeouts for more retries.

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,14 +8,14 @@ Capybara.run_server = false
 Capybara.default_driver = :cuprite
 Capybara.javascript_driver = :cuprite
 Capybara.app_host = ENV["APP_HOST"] || "http://localhost:3000"
-Capybara.default_max_wait_time = 20
+Capybara.default_max_wait_time = 10
 Capybara.register_driver(:cuprite) do |app|
   Capybara::Cuprite::Driver.new(app,
     browser_options: {
       'no-sandbox': nil,
       'disable-smooth-scrolling': true
     },
-    timeout: 30
+    timeout: 15
   )
 end
 
