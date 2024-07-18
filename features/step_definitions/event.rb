cce247bb2b71d9af965ef46229b9cb76f20e3800
Nizreenana <nizreenana17@gmail.com>
2024-07-18 17:37:58 +0200

    TEST : Comment or adapt a document with an existing document

--- features/step_definitions/event.rb
+++ features/step_definitions/event.rb
@@ -103,3 +103,12 @@ Quand("je sélectionne {string} dans le menu déroulant") do |option|
   click_on_icon('gloses .create-document')
 end
 
+Quand("j'essaie de créer le même commentaire pour {string}") do |title|
+  click_on_icon('close')
+  click_on_icon_next_to('focus', title)
+  click_on_icon('select-document')
+  document_list = all('.documentList', visible: true)
+  document_to_select = document_list.find { |document| document.text.include?("Yulia Commentaire") }
+  document_to_select.click
+end
+
