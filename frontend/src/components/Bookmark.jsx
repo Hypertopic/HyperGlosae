Hypertopic/HyperGlosae

commit 0b45d674c409366dcde4a66f858dd9b0c0e14571

FIX: Auth session should be persisted after refresh (fixes #163).

frontend/src/components/Bookmark.jsx
@@ -4,7 +4,7 @@ import { v4 as uuid } from 'uuid';
 
 function Bookmark({backend, id}) {
   const [isBookmarked, setIsBookmarked] = useState(false);
-  let user = backend.credentials.name;
+  let user = backend.user;
 
   const getBookmark = useCallback((id, user) =>
     backend.getView({view: 'bookmark', id: user, options: ['include_docs']})

frontend/src/components/FutureDocument.jsx
@@ -57,7 +57,7 @@ function FutureDocumentIcon({relatedTo, verb, setLastUpdate, backend}) {
     let _id = uuid().replace(/-/g, '');
     let doc = {
       _id,
-      editors: [backend.credentials.name],
+      editors: [backend.user],
       dc_creator: '<CREATOR>',
       dc_title: '<TITLE>',
       dc_issued: new Date(),

frontend/src/components/Menu.jsx
@@ -33,9 +33,12 @@ function Authentication({backend, user, setUser}) {
   let handleSubmit = (e) => {
     e.preventDefault();
     let credentials = Object.fromEntries(new FormData(e.target).entries());
-    backend.authenticate(credentials)
-      .then(() => setUser(credentials.name))
-      .catch(console.error);
+    backend.postSession(credentials)
+      .then((x) => {
+        if (x) {
+          setUser(x);
+        }
+      });
   };
 
   if (user) return (
@@ -44,7 +47,7 @@ function Authentication({backend, user, setUser}) {
         {user}
       </Dropdown.Toggle>
       <Dropdown.Menu>
-        <SignOutAction/>
+        <SignOutAction {...{setUser, backend}} />
       </Dropdown.Menu>
     </Dropdown>
   );

frontend/src/hyperglosae.js
@@ -1,10 +1,6 @@
-import {Buffer} from 'buffer';
-
 const service = '/api';
 
 function Hyperglosae(logger) {
-
-  this.credentials = {};
 
   this.getView = ({view, id, options = []}) =>
     fetch(`${
@@ -23,18 +19,9 @@ function Hyperglosae(logger) {
     fetch(`${service}/${id}`)
       .then(x => x.json());
 
-  let basicAuthentication = ({force}) => {
-    let {name, password} = this.credentials;
-    if (!force && !name && !password) return ({});
-    return ({
-      'Authorization': 'Basic ' + Buffer.from(`${name}:${password}`).toString('base64')
-    });
-  };
-
   this.putDocument = (doc, uri) =>
     fetch(`${service}/${uri || doc._id}`, {
       method: 'PUT',
-      headers: basicAuthentication({force: false}),
       body: JSON.stringify(doc)
     })
       .then(x => x.json())
@@ -49,7 +36,6 @@ function Hyperglosae(logger) {
   this.deleteDocument = ({_id, _rev}) =>
     fetch(`${service}/${_id}?rev=${_rev}`, {
       method: 'DELETE',
-      headers: basicAuthentication({ force: false })
     })
       .then(x => x.json())
       .then(x => {
@@ -63,7 +49,6 @@ function Hyperglosae(logger) {
   this.getDocumentMetadata = (id) =>
     fetch(`${service}/${id}`, {
       method: 'HEAD',
-      headers: basicAuthentication({ force: false })
     });
 
   this.putAttachment = (id, attachment, callback) =>
@@ -76,7 +61,6 @@ function Hyperglosae(logger) {
         fetch(`${service}/${id}/${attachment.name}`, {
           method: 'PUT',
           headers: {
-            ...basicAuthentication({ force: false }),
             // ETag is the header that carries the current rev.
             'If-Match': x.headers.get('ETag'),
             'Content-Type': attachment.type
@@ -86,19 +70,34 @@ function Hyperglosae(logger) {
       };
     });
 
-  this.authenticate = ({name, password}) => {
-    this.credentials = {name, password};
-    return fetch(`${service}`, {
-      method: 'GET',
-      headers: basicAuthentication({force: true})
+  this.getSession = () =>
+    fetch(`${service}/_session`)
+      .then(x => x.json())
+      .then(x => {
+        this.user = x.userCtx?.name;
+        return this.user;
+      });
+
+  this.postSession = ({name, password}) =>
+    fetch(`${service}/_session`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `name=${name}&password=${password}`
     })
       .then(x => x.json())
       .then(x => {
-        if (x.reason) {
-          this.credentials = {};
-          logger(x.reason);
-          throw new Error(x.reason);
-        }
+        if (!x.reason) return this.user = name;
+        logger(x.reason);
+      });
+
+  this.deleteSession = () => {
+    fetch(`${service}/_session`, {
+      method: 'DELETE'
+    })
+      .then(() => {
+        this.user = null;
       });
   };
 
@@ -125,7 +124,7 @@ function Hyperglosae(logger) {
   };
 
   this.refreshDocuments = (callback) => {
-    let id = this.credentials.name || 'PUBLIC';
+    let id = this.user || 'PUBLIC';
     this.getView({view: 'all_documents', id, options: ['include_docs']})
       .then((rows) => {
         callback(

frontend/src/App.jsx
@@ -25,6 +25,8 @@ function App() {
           setTypes(rows);
         }
       );
+    backend.getSession()
+      .then(setUser);
   }, []);
   return (
     <StrictMode>

frontend/src/components/SignOutAction.jsx
@@ -1,8 +1,14 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 
-function SignOutAction() {
+function SignOutAction({setUser, backend}) {
+
+  const handleSignOut = () => {
+    backend.deleteSession();
+    setUser();
+  };
+
   return (
-    <Dropdown.Item onClick={() => window.location.reload()}>
+    <Dropdown.Item onClick={handleSignOut}>
       Sign out
     </Dropdown.Item>
   );

frontend/vite.config.js
@@ -16,6 +16,11 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/_users/, ''),
       },
+      '/api/_session': {
+        target: 'http://localhost:5984/_session',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/_session/, ''),
+      },
       '/api': {
         target: 'http://localhost:5984/hyperglosae',
         changeOrigin: true,

settings/haproxy.cfg
@@ -30,6 +30,7 @@ frontend http-in
 backend couchdb
     option httpchk GET /_up
     http-check disable-on-404
+    http-request replace-path /api/_session /_session
     http-request replace-path /api/_users/(.*) /_users/\1
     http-request replace-path /api(/)?(.*) /hyperglosae/\2
     server couchdb1 backend:5984 check inter 5s

commit b3f69bc9463f594f5915056282080a44303ff8df

FIX: The user bookshelf should be refreshed after signing out.
Regression caused by 0b45d674c409366dcde4a66f858dd9b0c0e14571.

frontend/src/components/Bookmark.jsx
@@ -2,9 +2,8 @@ import { useState, useEffect, useCallback } from 'react';
 import { BookmarkFill } from 'react-bootstrap-icons';
 import { v4 as uuid } from 'uuid';
 
-function Bookmark({backend, id}) {
+function Bookmark({backend, user, id}) {
   const [isBookmarked, setIsBookmarked] = useState(false);
-  let user = backend.user;
 
   const getBookmark = useCallback((id, user) =>
     backend.getView({view: 'bookmark', id: user, options: ['include_docs']})
@@ -17,27 +16,17 @@ function Bookmark({backend, id}) {
         .then(bookmark => setIsBookmarked(!!bookmark))
         .catch(console.error);
     }
-  }, [user, id, backend, getBookmark]);
-
-  const createBookmark = () =>
-    backend.putDocument({
-      _id: uuid(),
-      editors: [user],
-      bookmark: id
-    });
-
-  const removeBookmark = () =>
-    getBookmark(id, user)
-      .then(bookmark => bookmark.doc)
-      .then(backend.deleteDocument);
+  }, [user, id, getBookmark]);
 
   const onBookmarkToggle = () => {
     if (!isBookmarked) {
-      createBookmark(user, id)
+      backend.putDocument({ _id: uuid(), editors: [user], bookmark: id })
         .then(() => setIsBookmarked(true))
         .catch(console.error);
     } else {
-      removeBookmark(id, user)
+      getBookmark(id, user)
+        .then(x => x.doc)
+        .then(backend.deleteDocument)
         .then(() => setIsBookmarked(false))
         .catch(console.error);
     }

frontend/src/components/DocumentList.jsx
@@ -15,11 +15,12 @@ function DocumentList({ relatedTo, verb, setSelectedDocument, setShowDocumentLis
   );
 
   useEffect(() => {
-    backend.refreshDocuments(
-      x => setUserDocuments(
-        x.filter(y => y && y._id !== relatedTo[0])
-      )
-    );
+    backend.getAllDocuments(user)
+      .then(x =>
+        setUserDocuments(
+          x.filter(y => y && y._id !== relatedTo[0])
+        )
+      );
   }, [user, backend, relatedTo]);
 
   return (

frontend/src/components/FutureDocument.jsx
@@ -29,7 +29,7 @@ function FutureDocument({relatedTo, setLastUpdate, backend, user}) {
         )}
         <FutureDocumentIcon
           relatedTo={selectedDocument ? [selectedDocument._id] : relatedTo}
-          {...{verb, setLastUpdate, backend}}
+          {...{verb, setLastUpdate, backend, user}}
         />
         {!fixedType && (
           <Link
@@ -50,14 +50,14 @@ function FutureDocument({relatedTo, setLastUpdate, backend, user}) {
   );
 }
 
-function FutureDocumentIcon({relatedTo, verb, setLastUpdate, backend}) {
+function FutureDocumentIcon({relatedTo, verb, setLastUpdate, backend, user}) {
   const navigate = useNavigate();
 
   let handleClick = async () => {
     let _id = uuid().replace(/-/g, '');
     let doc = {
       _id,
-      editors: [backend.user],
+      editors: [user],
       dc_creator: '<CREATOR>',
       dc_title: '<TITLE>',
       dc_issued: new Date(),

frontend/src/components/Menu.jsx
@@ -34,11 +34,7 @@ function Authentication({backend, user, setUser}) {
     e.preventDefault();
     let credentials = Object.fromEntries(new FormData(e.target).entries());
     backend.postSession(credentials)
-      .then((x) => {
-        if (x) {
-          setUser(x);
-        }
-      });
+      .then(setUser);
   };
 
   if (user) return (

frontend/src/components/OpenedDocuments.jsx
@@ -12,14 +12,14 @@ import DeleteDocumentAction from './DeleteDocumentAction';
 import Bookmark from './Bookmark';
 import LicenseCompatibility from './LicenseCompatibility';
 
-function OpenedDocuments({id, margin, metadata, parallelDocuments, hasSources, backend, setLastUpdate}) {
+function OpenedDocuments({id, margin, metadata, parallelDocuments, hasSources, backend, user, setLastUpdate}) {
   const marginMetadata = metadata.getDocument(margin);
   const marginLicense = marginMetadata?.dc_license;
   const sourceMetadata = metadata.focusedDocument;
   return (
     <Col className="lectern">
       <Row className ="runningHead">
-        <RunningHeadSource {...{id, metadata, hasSources, parallelDocuments, backend}} />
+        <RunningHeadSource {...{id, metadata, hasSources, parallelDocuments, backend, user}} />
         <RunningHeadMargin {...{parallelDocuments, margin, backend, setLastUpdate}}
           metadata={marginMetadata}
         />
@@ -50,14 +50,14 @@ function OpenedDocuments({id, margin, metadata, parallelDocuments, hasSources, b
   );
 }
 
-function RunningHeadSource({id, metadata, hasSources, parallelDocuments, backend}) {
+function RunningHeadSource({id, metadata, hasSources, parallelDocuments, backend, user}) {
   metadata = metadata.focusedDocument;
   if (parallelDocuments.isFromScratch) return (
     <Col className="main" />
   );
   return (
     <Col className="main">
-      <Bookmark {...{backend, id}} />
+      <Bookmark {...{backend, user, id}} />
       <BrowseTools {...{id}} editable={!hasSources} focusable={false} />
       <Metadata {...{metadata}} />
       <TypeBadge type={metadata?.type} />

frontend/src/hyperglosae.js
@@ -73,10 +73,7 @@ function Hyperglosae(logger) {
   this.getSession = () =>
     fetch(`${service}/_session`)
       .then(x => x.json())
-      .then(x => {
-        this.user = x.userCtx?.name;
-        return this.user;
-      });
+      .then(x => x.userCtx?.name);
 
   this.postSession = ({name, password}) =>
     fetch(`${service}/_session`, {
@@ -87,19 +84,12 @@ function Hyperglosae(logger) {
       body: `name=${name}&password=${password}`
     })
       .then(x => x.json())
-      .then(x => {
-        if (!x.reason) return this.user = name;
-        logger(x.reason);
-      });
+      .then(x => x.reason ? logger(x.reason) : name);
 
-  this.deleteSession = () => {
+  this.deleteSession = () =>
     fetch(`${service}/_session`, {
       method: 'DELETE'
-    })
-      .then(() => {
-        this.user = null;
-      });
-  };
+    });
 
   this.refreshMetadata = (id, callback) => {
     this.getView({view: 'metadata', id, options: ['include_docs']})
@@ -123,15 +113,9 @@ function Hyperglosae(logger) {
       );
   };
 
-  this.refreshDocuments = (callback) => {
-    let id = this.user || 'PUBLIC';
-    this.getView({view: 'all_documents', id, options: ['include_docs']})
-      .then((rows) => {
-        callback(
-          rows.map(x => x.doc)
-        );
-      });
-  };
+  this.getAllDocuments = (user) =>
+    this.getView({view: 'all_documents', id: user || 'PUBLIC', options: ['include_docs']})
+      .then((rows) => rows.map(x => x.doc));
 
   return this;
 }

frontend/src/routes/Bookshelf.jsx
@@ -9,13 +9,14 @@ function Bookshelf({backend, user}) {
   const [lastUpdate, setLastUpdate] = useState();
 
   useEffect(() => {
-    backend.refreshDocuments(setDocuments);
+    backend.getAllDocuments(user)
+      .then(setDocuments);
   }, [lastUpdate, user, backend]);
 
   return (
     <Container className="screen bookshelf">
       <DocumentsCards docs={documents} byRow={4} createOn={[]}
-        {...{setLastUpdate, backend}}
+        {...{setLastUpdate, backend, user}}
       />
     </Container>
   );

frontend/src/routes/Lectern.jsx
@@ -42,7 +42,7 @@ function Lectern({backend, user}) {
         </Col>
         <OpenedDocuments
           hasSources={metadata.forwardLinkedDocuments.length > 0}
-          {...{id, margin, metadata, parallelDocuments, backend, setLastUpdate}}
+          {...{id, margin, metadata, parallelDocuments, user, backend, setLastUpdate}}
         />
         <References active={!margin} createOn={[id]}
           {...{metadata, user, setLastUpdate, backend}}