06bf1bee6fd812c5d2cc74772f4b2b7eaa67077e
florianbnl <134014225+florianbnl@users.noreply.github.com>
2025-04-15 16:27:38 +0200

    FIX: Comment part of a scholium may include formatting (fixes #237).
    Co-Authored-By: Theo <34419764+SanKson7@users.noreply.github.com>
    Co-Authored-By: claragullotta <127398339+claragullotta@users.noreply.github.com>

--- frontend/src/components/FragmentComment.jsx
+++ frontend/src/components/FragmentComment.jsx
@@ -3,9 +3,18 @@ import '../styles/FragmentComment.css';
 function FragmentComment({ children, setHighlightedText }) {
   try {
     const citationRegex = /^\[.*\]\s*\n(.*)$/m;
-    if (citationRegex.test(children)) {
+    if (citationRegex.test(children[0])) {
       let [citation, comment] = children[0].split(/\n/);
       citation = citation.replace(/[[\]]/g, '');
+      const commentParts = [
+        comment,
+        ...children.slice(1)
+      ].map((part, index) => (
+        <span key={index}>
+          {part}
+        </span>
+      ));
+
       return <p
         onMouseEnter={
           e => {
@@ -25,7 +34,7 @@ function FragmentComment({ children, setHighlightedText }) {
         title="Highlight in document"
       >
         <span className="citation">{citation}</span>
-        {comment}
+        {commentParts}
       </p>;
     }
   } catch (e) {
