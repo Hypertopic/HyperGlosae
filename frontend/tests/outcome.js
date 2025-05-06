343f05f5c373553d20ac7e2e9e24c91274e59b43
gaydailo <gaydailo@LAPTOP-46PO3ID7.>
2025-05-06 16:02:30 +0200

    test outcome cypress language

--- frontend/tests/outcome.js
+++ frontend/tests/outcome.js
@@ -37,6 +37,10 @@ Alors("l'année de publication est {string}", (year) => {
   cy.get('.metadata > .edition').first().should('contain', year);
 });
 
+Alors("la langue est {string}", (language) => {
+  cy.get('.metadata > .language').first().should('contain', language);
+});
+
 Alors("la glose contient {string}", (text) => {
   cy.contains('.scholium .formatted-text', text).should('exist');
 });
