e8f7fbea4c1d3c6115d3c94e149da00d022db775
Lucas Sauron <sauronlucas@gmail.com>
2025-05-13 16:17:28 +0200

    Revert "started modification to manage delete reference test"
    This reverts commit 6e56afd408cc5931640a497795b18d70c24a32ef.

--- frontend/tests/event.js
+++ frontend/tests/event.js
@@ -1,140 +1,107 @@
-import { When as Quand } from "@badeball/cypress-cucumber-preprocessor";
+import { When as Quand } from '@badeball/cypress-cucumber-preprocessor';
 
 Quand("j'essaie de créer un nouveau document", () => {
-  cy.get(".create-document").click();
+  cy.get('.create-document').click();
 });
 
 Quand("j'essaie de créer un glose de type {string}", (option) => {
-  cy.get("#select-dropdown").select(option);
-  cy.get(".create-document").click();
+  cy.get('#select-dropdown').select(option);
+  cy.get('.create-document').click();
 });
 
 Quand("je me focalise sur {string}", (title) => {
-  cy.contains("span", title).parent().prev("a.focus").first().click();
+  cy.contains('span', title).parent().prev('a.focus').first().click();
 });
 
 Quand("j'essaie de remplacer les métadonnées de la glose par :", (metadata) => {
   cy.edit_metadata(metadata);
 });
 
-Quand(
-  "j'essaie de remplacer l'annotation du passage {int} par :",
-  (block_number, markdown) => {
-    let element = cy.get(
-      `.lectern>.row:nth-child(${
-        block_number + 1
-      })>.scholium>.content>.formatted-text`
-    );
-    cy.intercept("GET", "/api/*").as("getDocument");
-    element.click();
-    cy.wait("@getDocument");
-    cy.wait(1000);
-    cy.get("textarea")
-      .type("{selectAll} " + markdown)
-      .blur();
-  }
-);
+Quand("j'essaie de remplacer l'annotation du passage {int} par :", (block_number, markdown) => {
+  let element = cy.get(`.lectern>.row:nth-child(${block_number + 1})>.scholium>.content>.formatted-text`);
+  cy.intercept('GET', '/api/*').as('getDocument');
+  element.click();
+  cy.wait('@getDocument');
+  cy.wait(1000);
+  cy.get('textarea').type('{selectAll} ' + markdown).blur();
+});
 
 Quand("j'essaie de remplacer le contenu de la glose par :", (markdown) => {
-  cy.click_on_text("content", "<TEXT>");
-  cy.get("textarea")
-    .type("{selectAll} " + markdown.replaceAll(/[{}]/g, (x) => `{${x}}`))
-    .blur();
+  cy.click_on_text('content', '<TEXT>');
+  cy.get('textarea').type('{selectAll} ' + markdown.replaceAll(/[{}]/g, (x)=>`{${x}}`)).blur();
 });
 
 Quand("j'ajoute le document principal à ma bibliothèque", () => {
-  cy.get(".bookmark").click();
+  cy.get('.bookmark').click();
 });
 
 Quand("j'essaie d'accorder les droits d'édition à {string}", (userName) => {
-  cy.click_on_contextual_menu_item(
-    ".runningHead .scholium",
-    "Invite editors..."
-  );
-  cy.get(".modal-dialog input").type(userName);
-  cy.contains("button", "Invite").click();
+  cy.click_on_contextual_menu_item('.runningHead .scholium', 'Invite editors...');
+  cy.get('.modal-dialog input').type(userName);
+  cy.contains('button', 'Invite').click();
 });
 
 Quand("je choisis {string} comme type de glose", (pattern) => {
-  cy.get(".typeIcon").click();
-  cy.get("#searchType").type(pattern);
-  cy.get(".list-group-item").first().click();
+  cy.get('.typeIcon').click();
+  cy.get('#searchType').type(pattern);
+  cy.get('.list-group-item').first().click();
 });
 
 Quand("je survole le texte :", (text) => {
-  cy.contains('p[title="Highlight in document"]', text.trim()).trigger(
-    "mouseover"
-  );
+  cy.contains('p[title="Highlight in document"]', text.trim())
+    .trigger('mouseover');
 });
 
 Quand("je clique sur la référence temporelle {string} annotée", (timecode) => {
-  cy.contains("p", timecode).click();
+  cy.contains('p', timecode).click();
 });
 
 Quand("je sélectionne le fragment de texte :", (text) => {
   text = text.trim();
-  cy.contains("p", text).setSelection(text);
-  let paragraph = cy.contains(".main", text);
-  cy.click_on_contextual_menu_item(paragraph, "Comment the selected text...");
+  cy.contains('p', text).setSelection(text);
+  let paragraph = cy.contains('.main', text);
+  cy.click_on_contextual_menu_item(paragraph, 'Comment the selected text...');
 });
 
 Quand("je réutilise {string} comme glose", function (title) {
-  cy.get(".select-document").click();
+  cy.get('.select-document').click();
   cy.get('input[placeholder="Search documents"]').type(title);
-  cy.get(".existingDocument").first().click();
+  cy.get('.existingDocument').first().click();
 });
 
-Quand(
-  "je découpe la glose en passages numérotés et que je me focalise sur la glose",
-  () => {
-    cy.click_on_contextual_menu_item(
-      ".runningHead .scholium",
-      "Break into numbered passages"
-    );
-    cy.get(".focus").click();
-  }
-);
+Quand("je découpe la glose en passages numérotés et que je me focalise sur la glose", () => {
+  cy.click_on_contextual_menu_item('.runningHead .scholium', 'Break into numbered passages');
+  cy.get('.focus').click();
+});
 
-Quand(
-  "je remplace le contenu de la glose par ce qui suit et que je me focalise sur la glose :",
-  (markdown) => {
-    cy.click_on_text("content", "<TEXT>");
-    cy.get("textarea")
-      .type("{selectAll} " + markdown.replaceAll(/[{}]/g, (x) => `{${x}}`))
-      .blur();
-    cy.get(".focus").click();
-  }
-);
+Quand("je remplace le contenu de la glose par ce qui suit et que je me focalise sur la glose :", (markdown) => {
+  cy.click_on_text('content', '<TEXT>');
+  cy.get('textarea').type('{selectAll} ' + markdown.replaceAll(/[{}]/g, (x)=>`{${x}}`)).blur();
+  cy.get('.focus').click();
+});
 
 Quand("j'essaie d'ouvrir l'URI {string} reçue par courriel", (uri) => {
   cy.visit(uri);
 });
 
 Quand("je souhaite modifier le contenu du document principal", () => {
-  cy.get(".icon.edit").click();
-  cy.click_on_text("content");
+  cy.get('.icon.edit').click();
+  cy.click_on_text('content');
 });
 
-Quand("je supprime le lien entre le document principal et la référence", () => {
-  cy.get(".reference-dropdown").click();
-  cy.click_on_text("Delete reference").click();
+Quand("j'essaie de créer une glose en gardant {string} comme éditeur", (userName) => {
+  cy.get('.open-editor-list').click();
+  cy.get(`#editor-${userName}`).click();
+  cy.get(".create-document").click();
 });
-
-Quand(
-  "j'essaie de créer une glose en gardant {string} comme éditeur",
-  (userName) => {
-    cy.get(".open-editor-list").click();
-    cy.get(`#editor-${userName}`).click();
-    cy.get(".create-document").click();
-  }
-);
 
 Quand("j'essaie de créer une glose en gardant tous les éditeurs", () => {
-  cy.get(".open-editor-list").click();
+  cy.get('.open-editor-list').click();
   cy.get(`#select-all`).click();
   cy.get(".create-document").click();
 });
 
 Quand("je consulte les informations de création du document", function () {
-  cy.get(".info-icon-container").trigger("mouseover");
+  cy.get('.info-icon-container').trigger('mouseover');
 });
+
