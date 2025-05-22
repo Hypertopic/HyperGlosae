0121137d693c946eea48e7b4225a4606156bf802
Aurelien Benel <aurelien.benel@utt.fr>
2025-05-22 16:39:53 +0200

    REQUIREMENT: Language metadata should be displayed clearly and only for translations.

--- frontend/src/components/Metadata.jsx
+++ frontend/src/components/Metadata.jsx
@@ -41,6 +41,15 @@ function Metadata({metadata = {}, editable, backend, setLastUpdate}) {
   let format = (actors, prefix = '', suffix = '') =>
     actors && (prefix + [actors].flat().join(' & ') + suffix);
 
+  let capitalize = (name) =>
+    name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+
+  let formatTranslation = (translators = '', language = '') =>
+    (translators) &&
+      'Translated '
+        + (language && `in ${capitalize(language)} `)
+        + format(translators, 'by ', ', ');
+
   let getCaption = ({dc_title, dc_spatial}) => dc_title + (dc_spatial ? `, ${dc_spatial}` : '');
 
   if (!beingEdited) {
@@ -54,10 +63,9 @@ function Metadata({metadata = {}, editable, backend, setLastUpdate}) {
           {getCaption({dc_title, dc_spatial})} {format(dc_creator, '(', ')')},
         </span>
         <span className="edition">
-          {format(dc_translator, 'Translated by ', ', ')}
+          {formatTranslation(dc_translator, dc_language)}
           {dc_isPartOf ? <i>{dc_isPartOf}, </i> : ''}
           {dc_issued ? `${new Date(dc_issued.toString()).getFullYear()}` : ''}
-          {dc_language ? `, ${dc_language.charAt(0).toUpperCase() + dc_language.slice(1).toLowerCase()}` : ''}
         </span>
       </span>
     );
