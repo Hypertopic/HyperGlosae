740d4243baad03b3704b87b6ab0898206df304a1
Aurelien Benel <aurelien.benel@utt.fr>
2025-05-27 15:58:28 +0200

    REFACTORING: Passage parsing in the backend should be reusable.

--- backend/hyperglosae/src/views/lib/links.js
+++ backend/hyperglosae/src/views/lib/links.js
@@ -12,23 +12,30 @@ exports.getRelatedDocuments = ({isPartOf, links}) =>
       .map(x => parseReference(x).id)
   );
 
-exports.emitPassages = ({text, isPartOf, related}) => {
-  if (!text) return;
+const parseText = (text) => {
+  if (!text) return [];
   const PASSAGE = /{([^{]+)} ([^{]*)/g;
   let passages = [...text.matchAll(PASSAGE)];
   passages = (passages.length) ? passages : [[null, '0', text]];
-  passages.forEach(([_, rubric, passage]) => {
-    let parsed_rubric = rubric.match(/(?:(\d+)[:\., ])?(\d+) ?([a-z]?)/)
+  return passages.map(([_, rubric, passage]) => ({
+    rubric,
+    passage,
+    parsed_rubric: rubric.match(/(?:(\d+)[:., ])?(\d+) ?([a-z]?)/)
         .slice(1)
         .filter(x => !!x)
         .map(x => {
           let n = Number(x) ;
           return Number.isNaN(n) ? x : n;
-        });
+        })
+  }));
+}
+
+exports.emitPassages = ({text, isPartOf, related}) => {
+  parseText(text).forEach(({rubric, passage, parsed_rubric}) =>
     related.forEach((x) => {
       emit([x, ...parsed_rubric], { text: passage, isPartOf, rubric, _id: null });
- 	  });
-  });
+ 	  })
+  );
 }
 
 exports.emitIncludedDocuments = ({isPartOf, links}) => {
