8a39bc0f8315b46c4967f4151e6d6e7f9c278158
Aurelien Benel <aurelien.benel@utt.fr>
2025-07-30 21:00:43 +0200

    PROCESS: The acceptance tests should be run (closes #199)...
    ... with `cypress-cucumber-preprocessor` instead of `cucumber-capybara`.

--- features/step_definitions/context.rb
+++
@@ -1,7 +0,0 @@
-Soit("un document dont je ne suis pas l'auteur affiché comme glose") do
-  visit '/4e1a31e14b032f2fa9e161ee9b123456'
-  sign_in('bill', 'madhatter')
-  click_on_icon('create-document')
-  sign_out
-end
-
--- features/step_definitions/outcome.rb
+++
@@ -1,4 +0,0 @@
-Alors("l'image intégrée en source {string} est légendée par son titre : {string}") do |image, image_caption|
-  expect(find('.sources')).to have_content image_caption
-end
-
