c574b8eef2dbe2b45382f8f3bb59f05e14bc2391
Sacha <sacha.h.2005.51@gmail.com>
2026-04-07 15:22:50 +0200

    FIX : Linking an existing document should be clearer and more practical (fixes #383).

--- frontend/src/components/ExistingDocument.jsx
+++ frontend/src/components/ExistingDocument.jsx
@@ -1,9 +1,13 @@
 import Card from 'react-bootstrap/Card';
 import { useNavigate } from 'react-router';
+import {format } from 'date-fns';
 
 function ExistingDocument({ document, relatedTo, verb, setLastUpdate, backend }) {
   const navigate = useNavigate();
   const title = document.dc_title || 'Untitled document';
+  const isPartOf = document.dc_isPartOf || '';
+  const creator = document.dc_creator || '';
+  const issued = format(new Date(document.dc_issued), 'dd/MM/yyyy HH:mm') || '';
   const sourceChunksToBeLinked = (verb !== 'includes' && relatedTo.length)
     ? [{ verb, object: relatedTo[0] }]
     : relatedTo.map(object =>({ verb, object }));
@@ -33,11 +37,10 @@ function ExistingDocument({ document, relatedTo, verb, setLastUpdate, backend })
       })
       .catch(console.error);
   };
-
   return (
     <Card onClick={handleClick} className="existingDocument documentList">
       <Card.Body>
-        <span>{title}</span>
+        <span><strong>{title}</strong> - <i>{isPartOf} - {creator} - {issued}</i></span>
       </Card.Body>
     </Card>
   );
