4a11edbc6c458721192b8120d86fd12bf4ec87ab
ZHENG-Qi625 <zgandasuka@gmail.com>
2026-04-07 15:00:37 +0200

    Update FormattedText.jsx (Supprimer tous les commentaires)
    Co-authored-by: Nourhane Boudaoud <@NourhaneBoudaoud>, LI Zhenpeng <@LZPxka>

--- frontend/src/components/FormattedText.jsx
+++ frontend/src/components/FormattedText.jsx
@@ -6,7 +6,6 @@ import CroppedImage from './CroppedImage';
 import VideoComment from './VideoComment';
 import FragmentComment from './FragmentComment';
 
-// Extrait l'ID YouTube depuis un lien
 function getId(text) {
   if (!text) return null;
   const regExp = /^.*(?:youtube\.com\/watch\?v=|youtu\.be\/)([^\s&]{11})/;
@@ -14,7 +13,6 @@ function getId(text) {
   return match ? match[1] : null;
 }
 
-// Pour gérer les balises img qui pointent vers YouTube
 function embedVideo({ src }) {
   const videoId = getId(src);
   if (videoId) {
@@ -34,7 +32,6 @@ function FormattedText({ children, setHighlightedText, selectable, setSelectedTe
     }
   };
 
-  // Nouveau rendu pour <a> : transforme les liens YouTube purs en iframe
   const renderLinkOrVideo = ({ children, href }) => {
     const videoId = getId(href);
     if (videoId) {
