1e3ead9c99d96f5af05a5fb7beff25f3c88fc965
ZHENG-Qi625 <zgandasuka@gmail.com>
2026-04-07 14:50:35 +0200

    IMPROVEMENT: Reconnaissance automatique d'un lien YouTube pur (fixes #365)
    Co-authored-by : Nourhane Boudaoud <@NourhaneBoudaoud>, LI Zhenpeng <@LZPxka>

--- frontend/src/components/FormattedText.jsx
+++ frontend/src/components/FormattedText.jsx
@@ -6,7 +6,25 @@ import CroppedImage from './CroppedImage';
 import VideoComment from './VideoComment';
 import FragmentComment from './FragmentComment';
 
-function FormattedText({children, setHighlightedText, selectable, setSelectedText}) {
+// Extrait l'ID YouTube depuis un lien
+function getId(text) {
+  if (!text) return null;
+  const regExp = /^.*(?:youtube\.com\/watch\?v=|youtu\.be\/)([^\s&]{11})/;
+  const match = text.match(regExp);
+  return match ? match[1] : null;
+}
+
+// Pour gérer les balises img qui pointent vers YouTube
+function embedVideo({ src }) {
+  const videoId = getId(src);
+  if (videoId) {
+    const embedLink = `https://www.youtube.com/embed/${videoId}`;
+    return <iframe width="80%" height="300" src={embedLink} style={{ border: 0 }} allowFullScreen title="YouTube video" />;
+  }
+  return null;
+}
+
+function FormattedText({ children, setHighlightedText, selectable, setSelectedText }) {
 
   const handleMouseUp = () => {
     if (selectable) {
@@ -16,40 +34,36 @@ function FormattedText({children, setHighlightedText, selectable, setSelectedTex
     }
   };
 
-  return (<>
-    <ReactMarkdown
-      remarkPlugins={[remarkGfm, remarkDefinitionList, remarkUnwrapImages]}
-      components={{
-        img: (x) => embedVideo(x) || CroppedImage(x),
-        p: (x) => VideoComment(x)
-          || FragmentComment({...x, setHighlightedText})
-          || <p onMouseUp={handleMouseUp}>{x.children}</p>,
-        a: ({children, href}) => <a href={href}>{children}</a>
-      }}
-      remarkRehypeOptions={{
-        handlers: defListHastHandlers
-      }}
-    >
-      {children}
-    </ReactMarkdown>
-  </>);
-}
+  // Nouveau rendu pour <a> : transforme les liens YouTube purs en iframe
+  const renderLinkOrVideo = ({ children, href }) => {
+    const videoId = getId(href);
+    if (videoId) {
+      const embedLink = `https://www.youtube.com/embed/${videoId}`;
+      return <iframe width="80%" height="300" src={embedLink} style={{ border: 0 }} allowFullScreen title="YouTube video" />;
+    }
+    return <a href={href}>{children}</a>;
+  };
 
-function getId(text) {
-  const regExp = /^.*(?:youtube\.com\/watch\?v=|youtu\.be\/)([^\s&]{11})/;
-  const match = text.match(regExp);
-  return match ? match[1] : null;
-}
-
-function embedVideo({src}) {
-  const videoId = getId(src);
-  if (videoId) {
-    const embedLink = `https://www.youtube.com/embed/${videoId}`;
-    return (
-      <iframe width="80%" height="300" src={embedLink} frameBorder="0" allowFullScreen></iframe>
-    );
-  }
-  return null;
+  return (
+    <>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkDefinitionList, remarkUnwrapImages]}
+        components={{
+          img: (x) => embedVideo(x) || CroppedImage(x),
+          p: (x) =>
+            VideoComment(x) ||
+                        FragmentComment({ ...x, setHighlightedText }) ||
+                        <p onMouseUp={handleMouseUp}>{x.children}</p>,
+          a: renderLinkOrVideo
+        }}
+        remarkRehypeOptions={{
+          handlers: defListHastHandlers
+        }}
+      >
+        {children}
+      </ReactMarkdown>
+    </>
+  );
 }
 
 export default FormattedText;
