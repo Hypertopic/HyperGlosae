457fcfa504d4b3fab9241e7f4f5df957e90ca97d
nathannct <nathan.nicart@utt.fr>
2026-06-09 17:01:03 +0200

    PROCESS: Footprint evaluation should not wait for the EventSource to close.

--- requirements/impact_of_parallel_texts.js
+++ requirements/impact_of_parallel_texts.js
@@ -4,12 +4,9 @@
 
 module.exports = async (page) => {
 
-  await page.goto("37b4b9ba5cdb11ed887beb5c373fa643#09c906c6732b11ed89466ba197585f87", {
-    waitUntil: 'networkidle0'
-  });
+  await page.goto("37b4b9ba5cdb11ed887beb5c373fa643#09c906c6732b11ed89466ba197585f87");
   await page.waitForTimeout(10000);
   await page.scrollToEnd();
-  await page.waitForNetworkIdle();
   await page.waitForTimeout(7000);
 
 };
