Hypertopic/HyperGlosae

commit 1e79aa7920b61ea67a597b9b04421df6d527f3d9

IMPROVEMENT: Document menus should remain always visible (closes #284).

frontend/src/components/OpenedDocuments.jsx
@@ -6,7 +6,7 @@ import Metadata from './Metadata';
 import Type, { TypeBadge } from './Type';
 import Passage from './Passage';
 import License from './License';
-import DiscreeteDropdown from './DiscreeteDropdown';
+import VisibleDropdown from './VisibleDropdown';
 import InviteEditorsAction from '../menu-items/InviteEditorsAction';
 import BreakIntoPassagesAction from '../menu-items/BreakIntoPassagesAction';
 import DeleteDocumentAction from '../menu-items/DeleteDocumentAction';
@@ -66,13 +66,15 @@ function RunningHeadSource({id, metadata, parallelDocuments, backend, user}) {
   );
   return (
     <Col className="main position-relative" ref={target}>
-      <BookmarkFill />
-      <Metadata {...{metadata}} />
-      <DiscreeteDropdown>
+      <VisibleDropdown>
         <ToggleBookmarkAction {...{id, backend, user}}/>
         <EditDocumentAction {...{id}} />
         <ViewHistoryAction {...{metadata, target, backend}} />
-      </DiscreeteDropdown>
+      </VisibleDropdown>
+      <div className="headerBox">
+        <BookmarkFill className="icon" />
+        <Metadata {...{metadata}} />
+      </div>
       <TypeBadge type={metadata?.type} />
     </Col>
   );
@@ -82,15 +84,17 @@ function RunningHeadMargin({id, metadata, parallelDocuments, margin, setRawEditM
   const isFromScratch = parallelDocuments.isFromScratch;
   if (Object.keys(metadata).length) return (
     <Col xs={5} className="scholium position-relative">
-      <BrowseTools id={metadata._id} closable={!parallelDocuments.isFromScratch} />
-      <DiscreeteDropdown>
+      <VisibleDropdown>
         <InviteEditorsAction {...{backend, metadata, setLastUpdate}} />
         <BreakIntoPassagesAction {...{parallelDocuments, margin, backend, setLastUpdate}} />
         <EditRawDocumentAction {...{setRawEditMode}} />
         <DeleteDocumentAction {...{metadata, isFromScratch, backend, setLastUpdate}} />
         <DeleteReferenceToDocumentAction {...{id, margin, backend, metadata, content, setLastUpdate}} />
-      </DiscreeteDropdown>
-      <Metadata editable={true} {...{backend, metadata, setLastUpdate}} />
+      </VisibleDropdown>
+      <div className="headerBox">
+        <BrowseTools id={metadata._id} closable={!parallelDocuments.isFromScratch} />
+        <Metadata editable={true} {...{backend, metadata, setLastUpdate}} />
+      </div>
       <Type editable={true} {...{backend, metadata}}/>
     </Col>
   );

frontend/src/styles/Lectern.css
@@ -45,7 +45,7 @@
 .lectern .row:first-of-type .scholium {
   border-radius: 0px 10px 0px 0px;
   justify-content: flex-end;
-  text-align: right;
+  text-align: left;
 }
 
 .lectern .row:last-of-type .scholium {
@@ -58,6 +58,10 @@
 
 .runningHead {
   color: crimson;
+}
+
+.headerBox {
+  margin-right: 24px !important;
 }
 
 .icon,

frontend/src/components/VisibleDropdown.jsx
@@ -1,0 +1,21 @@
+import '../styles/DiscreeteDropdown.css';
+
+import { Dropdown } from 'react-bootstrap';
+import { List } from 'react-bootstrap-icons';
+
+function VisibleDropdown({children}) {
+
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="secondary" size="sm" className="no-caret top-right toggle">
+        <List />
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {children}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default VisibleDropdown;
+

frontend/src/styles/DiscreeteDropdown.css
@@ -12,3 +12,12 @@
   cursor: pointer;
 }
 
+.no-caret::after {
+  display: none !important;
+}
+
+.top-right {
+  position: absolute;
+  top: 0;
+  right: 0;
+}

frontend/tests/context.js
@@ -216,7 +216,7 @@ Soit("ayant les métadonnées", (metadata) => {
       expect(actualMetadata).to.have.property(key, value);
     });
   });
-  cy.get('.scholium>.icon.focus').click();
+  cy.get('.scholium .icon.focus').click();
 });
 
 Soit("je choisis {string} comme type de reférence", (gloseType) => {

commit 8532b4449e070628730cd861d7b290f184be1ac6

FIX: The "focused document" icon should be displayed as passive.
Regression caused by 1e79aa7.

frontend/src/components/OpenedDocuments.jsx
@@ -73,7 +73,7 @@ function RunningHeadSource({id, metadata, parallelDocuments, backend, user}) {
         <ViewHistoryAction {...{metadata, target, backend}} />
       </VisibleDropdown>
       <div className="headerBox">
-        <BookmarkFill className="icon" />
+        <BookmarkFill className="passiveIcon" />
         <Metadata {...{metadata}} />
       </div>
       <TypeBadge type={metadata?.type} />

frontend/src/styles/Lectern.css
@@ -67,7 +67,10 @@
 .icon,
 .icon:hover {
   color: crimson;
-  text-decoration: none;
+}
+
+.icon,
+.passiveIcon {
   margin-right: .5rem;
 }
 